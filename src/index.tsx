7e9810db834e79fdfc4dba8f69885e4d493925f8
Radu Dascalu <radu.dascalu@pm.me>
2024-08-30 13:09:33 +0100

    Add path alias

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import { render } from "solid-js/web";
 
 import "virtual:uno.css";
 
-import App from './App';
+import App from "~/App";
 
-const root = document.getElementById('root');
+import "~/styles/app.css";
+
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 import UnoCSS from "unocss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     devtools({
       autoname: true,
       locator: {
