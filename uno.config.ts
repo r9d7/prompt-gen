8991b1de03f522e772c7023fe30941cf49a5dc33
Radu Dascalu <radu.dascalu@pm.me>
2024-08-30 15:25:32 +0100

    Add default styles

--- uno.config.ts
+++ uno.config.ts
@@ -11,7 +11,28 @@ export default defineConfig({
     presetUno(),
     presetWebFonts({
       provider: "bunny",
+      fonts: {
+        sans: {
+          name: "Public Sans",
+          weights: ["400", "700"],
+        },
+      },
     }),
   ],
+  theme: {
+    colors: {
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+    },
+  },
   transformers: [transformerVariantGroup(), transformerDirectives()],
 });
